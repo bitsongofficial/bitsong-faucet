e619fdd52f41cc5cb561c08a904dd8d4e928ba37
angelorc <angelo@rcstart.it>
2022-05-28 13:26:35 +0200

    try to fix multicoin

--- src/constants/faucetConfig.ts
+++ src/constants/faucetConfig.ts
@@ -3,21 +3,6 @@ import { Constants } from '@bitsongjs/sdk';
 export const faucetAmount = [{
   denom: "ubkn",
   amount: '10000000',
-},{
-  denom: "ueur",
-  amount: '10000000',
-},{
-  denom: "uchf",
-  amount: '10000000',
-},{
-  denom: "ugbp",
-  amount: '10000000',
-},{
-  denom: "uusd",
-  amount: '10000000',
-},{
-  denom: "uyen",
-  amount: '10000000',
 }];
 
 export const defaultFee = {
